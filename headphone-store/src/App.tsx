f91b1d84739ac7ce005d566874777961aa411b89
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2024-03-30 14:56:40 +0300

    add routing

--- headphone-store/src/App.tsx
+++ headphone-store/src/App.tsx
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
 import './App.css';
+import { HeadphoneList } from './app/HeadphoneList/HeadphoneList';
+import { Basket } from './app/Basket/Basket';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-      <h1>Hello everyone!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="qpick/basket" element={<Basket />} />
+        <Route path="qpick" element={<HeadphoneList />} />
+        <Route path="*" element={<Navigate to="/qpick" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
